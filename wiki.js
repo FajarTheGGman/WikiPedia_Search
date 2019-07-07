d46394c9ff4bd8f21d691ce405e9ee87aa9579c2
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-07-07 12:20:32 +0700

    Create wiki.js

---
+++ wiki.js
@@ -0,0 +1,36 @@
+var req = require("request");
+var rl = require("readline");
+var style = require("chalk");
+var j = require("jsome")
+var ch = require("cheerio");
+var sh = require("shelljs");
+
+console.log(style.blue("       .-.         _______                         "))                        
+console.log(style.blue("      {}``;    |==|_______D   [ WikiPedia Search ] "))                       
+console.log(style.blue("      / ('           |              [ By ]         "))                     
+console.log(style.blue("     /  |            |         [ Fajar Firdaus ]   "))                  
+console.log(style.blue("     _)_]]           |                             "))
+
+j({"Coder" : "Fajar Firdaus", "FB" : "Fajar Firdaus", "IG" : "fajar_firdaus_7", "YT" : "iTech7732"})
+                    
+const t = rl.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+var q = style.bgCyan("[ Input ] >_ ");
+
+t.question(q, (user) => {
+    req("https://en.wikipedia.org/wiki/" + `${user}`, (err,res,body) => {
+        var parse = ch.load(body);
+        if(res.statusCode == 200){
+            console.log(style.green("[ Result ]"))
+            console.log(style.bgCyan(parse("p").text()))
+            console.log(style.green("[========================================[Result]====================================]"))
+        }else if(res.statusCode == 404){
+            console.log(style.red("[!] Keyword Not Found"))
+        }else if(res.statusCode == 501){
+            console.log(style.red("[!] Connection Error Please Check Your Connection"))
+        }
+    })
+})
